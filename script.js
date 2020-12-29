e84266a41af4e3e4b2b617a61d9a46e40895dca2
Eddy Guo <eddyyanruguo@gmail.com>
2020-12-29 16:54:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let locationIcon = document.querySelector(".weather-icon");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=b86fd8f7708187a762619d1377a88ec7`;
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { temp } = data.main;
+          const { description } = data.weather[0];
+          const { icon } = data.weather[0];
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = description;
+          locationTimezone.textContent = data.name;
+        });
+    });
+  } else {
+    h1.textContent = "Please enable location";
+  }
+});
